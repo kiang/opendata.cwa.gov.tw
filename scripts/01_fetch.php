da323e43495a013150a1612eb008d7672957e36a
kiang <kiange@gmail.com>
2025-07-31 15:58:50 +0800

    change paths

--- scripts/01_fetch.php
+++ scripts/01_fetch.php
@@ -6,7 +6,7 @@ $data = json_decode(file_get_contents('https://opendata.cwa.gov.tw/api/v1/rest/d
 
 foreach ($data['records']['Earthquake'] as $record) {
     $y = substr($record['EarthquakeNo'], 0, 3);
-    $targetPath = $basePath . '/raw/' . $y;
+    $targetPath = $basePath . '/docs/cases/' . $y;
     if (!file_exists($targetPath)) {
         mkdir($targetPath, 0777, true);
     }
